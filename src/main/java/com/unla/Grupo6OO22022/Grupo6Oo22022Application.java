8fdbad463a6c9d04aff5ffb2388d3cb66997aa2a
PabloDL <pablo.delafore@hotmail.com>
2022-05-14 12:34:53 -0300

    Proyecto base

---
+++ src/main/java/com/unla/Grupo6OO22022/Grupo6Oo22022Application.java
@@ -0,0 +1,13 @@
+package com.unla.Grupo6OO22022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo6Oo22022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo6Oo22022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/Grupo6OO22022/Grupo6Oo22022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.Grupo6OO22022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo6Oo22022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
